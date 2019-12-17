e6dc399c3ff1786ee1e9a1b06cbe7cbd579e984d
graham <Graham.Wilson@cern.ch>
2019-12-17 16:42:26 -0600

    Some fixes to Ex2a.sh

--- Ex2a.sh
+++ Ex2a.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #
 # Ex2.sh
-# Example 2 of a shell script. Do something semi-automatically using 
+# Example 2 of a shell script. Do something semi-automatically using
 # a script rather than typing the details on the command line.
 # (I usually call this cl.sh)
 #
 # Also add some information
 #
-# Input argument - defaults to "quadratic"
+# Input argument. Gets set to what you specify - but if not present defaults to "quadratic"
 TARGET=${1-quadratic}
 echo "Compile and linking "${TARGET}.f
 gfortran -o ${TARGET} ${TARGET}.f
-echo "Created executable is quadratic"
+echo "Created executable is "${TARGET}
 echo "It can be executed by typing ./"${TARGET}
 
 exit
